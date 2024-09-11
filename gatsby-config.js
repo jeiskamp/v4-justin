80386eda8c4590cc94e48db6d1b376ea2b62a0d1
Justin <eiskamp7335@gmail.com>
2024-09-11 12:39:53 -0500

    Jeiskamp

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ module.exports = {
   siteMetadata: {
     title: 'Justin Eiskamp',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Justin Eiskamp is a software engineer who specializes in building (and occasionally designing) digital experiences.',
+    siteUrl: 'https://jeiskamp.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@x',
   },
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Justin Eiskamp',
+        short_name: 'Justin Eiskamp',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
